1b03509c88c91f1efacdc06b336b78e2d80e7d9e
clownlessmode <clownessmode@bk.ru>
2025-05-14 10:32:14 +0700

    fix

--- src/5_shared/api/api.ts
+++ src/5_shared/api/api.ts
@@ -15,7 +15,7 @@ api.interceptors.response.use(
     console.log(">>> axios interceptor caught error", err.response?.status);
     if (err.response?.status === 401) {
       console.log("401", useSessionStore.getState().session);
-      useSessionStore.getState().clearSession();
+      // useSessionStore.getState().clearSession();
       window.location.replace("/login");
     }
     return Promise.reject(err);
