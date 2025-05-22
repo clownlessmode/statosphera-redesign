f9b4757ae5b3b218c6238dee4cda1fdb996e1105
clownlessmode <clownessmode@bk.ru>
2025-05-22 14:40:31 +0700

    fix rows count

--- src/3_features/reports/download/ui/download-report.tsx
+++ src/3_features/reports/download/ui/download-report.tsx
@@ -13,11 +13,14 @@ import {
 import { useFiltersStore } from "@widgets/report/sheet/model/filters-store";
 import { useState } from "react";
 import { CardContent, CardDescription, CardTitle } from "@shared/ui/card";
+import { formatNumber } from "@shared/constants/table-columns";
+
 const DownloadReport = ({ rows }: { rows: number }) => {
   const { downloadReport } = useDownloadReportController();
   const { getApiPayload } = useFiltersStore();
   const [isOpen, setIsOpen] = useState(false);
   const payload = getApiPayload();
+
   const handleDownloadReport = async (typeFile: "csv" | "excel") => {
     await downloadReport({
       ...payload,
@@ -28,6 +31,10 @@ const DownloadReport = ({ rows }: { rows: number }) => {
     });
     setIsOpen(false);
   };
+
+  const isExcelDisabled = rows > 500000;
+  const isAllDisabled = rows > 7000000;
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
@@ -38,42 +45,74 @@ const DownloadReport = ({ rows }: { rows: number }) => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Скачать отчет</DialogTitle>
-          <DialogDescription>Выберите формат и тип отчета</DialogDescription>
+          <DialogDescription>
+            Выберите формат и тип отчета <br /> Кол-во строк: {formatNumber(rows)}
+          </DialogDescription>
         </DialogHeader>
 
         <DialogFooter className="flex flex-row gap-2 w-full">
           <Button
-            disabled={rows > 500000}
+            disabled={isExcelDisabled || isAllDisabled}
             className="w-full bg-green-500"
             onClick={() => handleDownloadReport("excel")}
           >
             Excel
           </Button>
           <Button
+            disabled={isAllDisabled}
             className="w-full bg-rose-500"
             onClick={() => handleDownloadReport("csv")}
           >
             CSV
           </Button>
         </DialogFooter>
-        {rows > 500000 && (
+
+        {(isExcelDisabled || isAllDisabled) && (
           <CardContent className="bg-background p-0 m-0 border-none">
             <CardTitle className="mb-1 flex flex-row items-center gap-1">
-              Ограничение на экспорт в Excel
+              {isAllDisabled
+                ? "Экспорт данных ограничен"
+                : "Ограничение на экспорт в Excel"}
               <Sparkles className="size-4 text-primary" />
             </CardTitle>
             <CardDescription>
-              В текущей выборке содержится более 500 000 строк данных. Экспорт
-              таких объёмов в Excel невозможен из-за технических ограничений
-              формата.
-              <br />
-              <br />
-              Для работы с большими данными рекомендуем:
-              <ul className="list-disc pl-5 mt-2 space-y-1">
-                <li>Применить дополнительные фильтры для уменьшения выборки</li>
-                <li>Использовать группировку данных перед экспортом</li>
-                <li>Экспортировать данные частями по определённым периодам</li>
-              </ul>
+              {isAllDisabled ? (
+                <>
+                  В текущей выборке содержится более 7 000 000 строк данных.
+                  Экспорт таких объёмов невозможен из-за ограничений системы.
+                  <br />
+                  <br />
+                  Для работы с данными рекомендуем:
+                  <ul className="list-disc pl-5 mt-2 space-y-1">
+                    <li>
+                      Применить дополнительные фильтры для уменьшения выборки
+                    </li>
+                    <li>Использовать группировку данных</li>
+                    <li>Разделить данные на несколько периодов</li>
+                    <li>
+                      Обратиться в техническую поддержку за альтернативными
+                      вариантами экспорта
+                    </li>
+                  </ul>
+                </>
+              ) : (
+                <>
+                  В текущей выборке содержится более 500 000 строк данных.
+                  Экспорт таких объёмов в Excel невозможен из-за технических
+                  ограничений формата.
+                  <br />
+                  <br />
+                  Для работы с большими данными рекомендуем:
+                  <ul className="list-disc pl-5 mt-2 space-y-1">
+                    <li>
+                      Применить дополнительные фильтры для уменьшения выборки
+                    </li>
+                    <li>Использовать группировку данных перед экспортом</li>
+                    <li>Экспортировать данные в формате CSV</li>
+                    <li>Разделить данные на несколько периодов</li>
+                  </ul>
+                </>
+              )}
             </CardDescription>
           </CardContent>
         )}
