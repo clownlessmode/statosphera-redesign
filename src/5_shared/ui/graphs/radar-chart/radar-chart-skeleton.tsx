ea567229dfdfca7a885b93403b16d3931600c20d
clownlessmode <clownessmode@bk.ru>
2025-06-25 13:44:51 +0700

    Исправлен скелетон процента выполнения плана

--- src/5_shared/ui/graphs/radar-chart/radar-chart-skeleton.tsx
+++ src/5_shared/ui/graphs/radar-chart/radar-chart-skeleton.tsx
@@ -1,6 +1,6 @@
 const RadarChartSkeleton = () => (
   <div className="w-full h-full flex items-center justify-center mx-auto">
-    <div className="w-[70%] aspect-square rounded-full bg-muted-foreground animate-pulse" />
+    <div className="h-[70%] aspect-square rounded-full bg-muted-foreground animate-pulse" />
   </div>
 );
 export default RadarChartSkeleton;
