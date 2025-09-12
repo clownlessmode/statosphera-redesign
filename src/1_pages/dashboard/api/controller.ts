69ac3d90ee5e9577eb392388ab63c577b4e03dba
clownlessmode <clownessmode@bk.ru>
2025-09-12 17:07:09 +0700

    fix

--- src/1_pages/dashboard/api/controller.ts
+++ src/1_pages/dashboard/api/controller.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 
 import { GetWeeklyRevenueResponse } from "./types";
 import { DashboardService } from "./service";
@@ -7,10 +7,13 @@ export const useDashboard = () => {
   const dashboard = useQuery<GetWeeklyRevenueResponse>({
     queryKey: ["dashboard"],
     queryFn: () => DashboardService.getAllData(),
+    refetchInterval: 10000, // Ревалидация каждую минуту (60 секунд)
+    placeholderData: keepPreviousData, // Показывать старые данные во время обновления
   });
 
   return {
     dashboard: dashboard.data,
     isDashboardLoading: dashboard.isLoading,
+    isDashboardFetching: dashboard.isFetching,
   };
 };
