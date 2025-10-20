1b39ea8a698ac88f83d76e3d2e894d0937de8b90
clownlessmode <clownessmode@bk.ru>
2025-10-20 13:17:12 +0700

    Final config update

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.371-de26c1a";
+export const VERSION = "1.0.372-e9123a7";
 
 export const VERSION_INFO = {
-  version: "1.0.371-de26c1a",
-  commitHash: "de26c1a",
+  version: "1.0.372-e9123a7",
+  commitHash: "e9123a7",
   commitDate: "2025-10-20",
-  commitCount: "371",
+  commitCount: "372",
   branch: "test",
   buildDate: "2025-10-20",
 };
