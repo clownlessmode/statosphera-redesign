2a47d2f609433829b286c99320957a1a1814ac41
clownlessmode <clownessmode@bk.ru>
2025-10-22 10:21:29 +0700

    trest sttstasfjsjn puki kaki kakashechki

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.396-865c6eb";
+export const VERSION = "1.0.398-740cc9a";
 
 export const VERSION_INFO = {
-  version: "1.0.396-865c6eb",
-  commitHash: "865c6eb",
-  commitDate: "2025-10-20",
-  commitCount: "396",
+  version: "1.0.398-740cc9a",
+  commitHash: "740cc9a",
+  commitDate: "2025-10-21",
+  commitCount: "398",
   branch: "test",
   buildDate: "2025-10-21",
 };
