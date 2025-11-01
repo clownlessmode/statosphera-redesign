9a3dfe0ee5092db7a9fb4fe2cba34bdb1dca2d62
clownlessmode <clownessmode@bk.ru>
2025-11-01 15:20:56 +0700

    feat: добавлена модалка с полной информацией о товаре Ярче

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.437-01462d1";
+export const VERSION = "1.0.438-6061f5d";
 
 export const VERSION_INFO = {
-  version: "1.0.437-01462d1",
-  commitHash: "01462d1",
+  version: "1.0.438-6061f5d",
+  commitHash: "6061f5d",
   commitDate: "2025-11-01",
-  commitCount: "437",
+  commitCount: "438",
   branch: "test",
   buildDate: "2025-11-01",
 };
