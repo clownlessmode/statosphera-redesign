a8d56ca51909194176a6f3314effd3bd634c6161
clownlessmode <clownessmode@bk.ru>
2025-11-01 15:23:21 +0700

    chore: обновлен config.ts

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.438-6061f5d";
+export const VERSION = "1.0.439-937e587";
 
 export const VERSION_INFO = {
-  version: "1.0.438-6061f5d",
-  commitHash: "6061f5d",
+  version: "1.0.439-937e587",
+  commitHash: "937e587",
   commitDate: "2025-11-01",
-  commitCount: "438",
+  commitCount: "439",
   branch: "test",
   buildDate: "2025-11-01",
 };
