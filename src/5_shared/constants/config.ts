fa2c407e6fa79e716e322a43703750a251a57864
clownlessmode <clownessmode@bk.ru>
2025-11-18 10:14:04 +0700

    fix

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.450-f57fbb5";
+export const VERSION = "1.0.472-64cb093";
 
 export const VERSION_INFO = {
-  version: "1.0.450-f57fbb5",
-  commitHash: "f57fbb5",
-  commitDate: "2025-11-06",
-  commitCount: "450",
+  version: "1.0.472-64cb093",
+  commitHash: "64cb093",
+  commitDate: "2025-11-13",
+  commitCount: "472",
   branch: "test",
-  buildDate: "2025-11-13",
+  buildDate: "2025-11-17",
 };
