c58d291b9cdbb3f05de12c8c07ca4c10a6b16d7b
clownlessmode <clownessmode@bk.ru>
2025-11-19 15:50:22 +0700

    super mega important fix

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.473-f92a170";
+export const VERSION = "1.0.486-60fea97";
 
 export const VERSION_INFO = {
-  version: "1.0.473-f92a170",
-  commitHash: "f92a170",
-  commitDate: "2025-11-17",
-  commitCount: "473",
-  branch: "test",
-  buildDate: "2025-11-18",
+  "version": "1.0.486-60fea97",
+  "commitHash": "60fea97",
+  "commitDate": "2025-11-19",
+  "commitCount": "486",
+  "branch": "",
+  "buildDate": "2025-11-19"
 };
