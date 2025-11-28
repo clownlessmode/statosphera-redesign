c8ccecf9cafbae5c6b3388260fafc871e064ca81
clownlessmode <clownessmode@bk.ru>
2025-11-28 13:23:22 +0700

    add new tarot phrases

--- src/5_shared/constants/config.ts
+++ src/5_shared/constants/config.ts
@@ -1,10 +1,10 @@
-export const VERSION = "1.0.474-fa2c407";
+export const VERSION = "1.0.520-500b6a8";
 
 export const VERSION_INFO = {
-  version: "1.0.474-fa2c407",
-  commitHash: "fa2c407",
-  commitDate: "2025-11-18",
-  commitCount: "474",
+  version: "1.0.520-500b6a8",
+  commitHash: "500b6a8",
+  commitDate: "2025-11-27",
+  commitCount: "520",
   branch: "test",
-  buildDate: "2025-11-19",
+  buildDate: "2025-11-28",
 };
